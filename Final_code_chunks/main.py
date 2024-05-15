d327d685af4b3cbf1e5d604fbfbebdb964478f0d
kenzaaaa222 <111131499+kenzaaaa222@users.noreply.github.com>
2024-05-15 17:58:30 -0400

    Update main.py

--- Final_code_chunks/main.py
+++ Final_code_chunks/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from PIL import Image
 from Crypto import Random
 from Crypto.Cipher import AES
@@ -11,29 +12,33 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 import argparse
 
 
+
 def main():
     parser=argparse.ArgumentParser(prog='kabb',description='new kabb command to encrypt an image')
     parser.add_argument('-n','--image',metavar='IMAGE_FILE',help='name of the image file to encrypt',required=False)
+    parser.add_argument('-f','--file',metavar='FILE_PATH',help='name of the file path')
     parser.add_argument('-enc','--encrypt',action='store_true',help='encrypt the image')  
-    parser.add_argument('-g','--generate_key',action='store_true',help='generate shared key using diffie-hellman')  
-    parser.add_argument('-f','--file',metavar='KEY_FILE',help='encryption file')   
-    parser.add_argument('-dec','--decrypt',action='store_true',help='decrypt the image')     
+    parser.add_argument('-g','--generate_key',action='store_true',help='generate shared key using diffie-hellman')    
+    parser.add_argument('-dec','--decrypt',action='store_true',help='decrypt the image')   
     args=parser.parse_args()
     
     if args.encrypt:
-        shared_key=int(input("enter you shared secret key :"))
-        if shared_key:
-           import image_encryption
+        print("*Please make sure your shared key is an integer ")
+        shared_key=input("enter you shared secret key :")
+        if shared_key :
+           import enc_test
            enc_test.encryption(args.image,shared_key)
-        else:
+        else: 
             print("if you do not have a key type -g to generate it ")
+            
     
     if args.generate_key:
         import Diffie_Hellman
-        Diffie_Hellman.key_generation()
+        #Diffie_Hellman.key_generation()
     if args.decrypt:
         import image_decryption
-        image_decryption(args.image,args.file)
+        image_decryption.image_decryption(args.image,args.file)
+        
           
 if __name__=="__main__":
     main()
